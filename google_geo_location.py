943eb18334df9ddf5d7bb29890f4bb777e12f083
Ray Valiani <rsvaliani@gmail.com>
2018-03-26 09:23:46 -0500

    moved personal info to cfg

--- google_geo_location.py
+++ google_geo_location.py
@@ -5,12 +5,19 @@ with open("google_api_keys.yml","r") as ymlfile:
 	cfg= yaml.load(ymlfile)
 
 api_key=cfg["map_api_key"]
+
+with open("my_info.yml","r") as ymlfile:
+	cfg= yaml.load(ymlfile)
+
+address=cfg["address"]
+
+
 url="https://maps.googleapis.com/maps/api/geocode/json"
 
 response= requests.get(
 	url,
 	headers={"Accept":"application/json"},
-	params={"address": "2300 winners cir, springfield, il","key":api_key}
+	params={"address": address,"key":api_key}
 	)
 
 data=response.json()
