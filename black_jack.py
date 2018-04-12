974ac83d26bbf1abcfd0b464101b12e3921fb8a6
Ray Valiani <ray.valiani@tsi.com>
2018-04-12 15:18:57 -0500

    Create a testing template and a skeleton to test Cards and Desk in black_jack

--- black_jack.py
+++ black_jack.py
@@ -66,55 +66,56 @@ class Deck:
 		return Deck._deal(self,num)
 
 
-computer_wins=0
-user_wins=0
+if __name__ == '__main__':
+	computer_wins=0
+	user_wins=0
 
-print(colored(figlet_format("Black Jack 1999"),color="white"))
+	print(colored(figlet_format("Black Jack 1999"),color="white"))
 
-while  True:
-	computer_won=False
-	user_won=False
-	the_deck=Deck(BlackJackCard)
-	the_deck.shuffle()
-	computer = the_deck.deal_card()
-	user=the_deck.deal_card()
-	computer_value=sum([item.get_black_jack_value() for item in computer])
-	user_value=sum([item.get_black_jack_value() for item in user])
+	while  True:
+		computer_won=False
+		user_won=False
+		the_deck=Deck(BlackJackCard)
+		the_deck.shuffle()
+		computer = the_deck.deal_card()
+		user=the_deck.deal_card()
+		computer_value=sum([item.get_black_jack_value() for item in computer])
+		user_value=sum([item.get_black_jack_value() for item in user])
 
-	print(f"You have {user} which has a value of {user_value}")
-	print(f"The computer has {computer} which has a value of {computer_value}")
+		print(f"You have {user} which has a value of {user_value}")
+		print(f"The computer has {computer} which has a value of {computer_value}")
 
-	while computer_value < BLACKJACK and user_value < BLACKJACK: 
-		if input("Would you like to  draw (y/n) ") == 'y':
-			user+=the_deck.deal_card()
-			user_value=sum([item.get_black_jack_value() for item in user])
-			user_value_ace_1=sum([item.get_black_jack_value(1) for item in user])
-			if user_value > BLACKJACK:
-				user_value=user_value_ace_1
-			print(f"You now have {user} your blackjack total is {user_value}")
-		
-		if (computer_value < (BLACKJACK - 4) and user_value < (BLACKJACK+1)) or (computer_value < user_value and user_value <= BLACKJACK):
-			computer+=the_deck.deal_card()
-			computer_value=sum([item.get_black_jack_value() for item in computer])
-			computer_value_ace_1=sum([item.get_black_jack_value(1) for item in computer])
-			if computer_value > BLACKJACK:
-				computer_value=computer_value_ace_1
-			print(f"The computer has {computer} his blackjack total is {computer_value}")
+		while computer_value < BLACKJACK and user_value < BLACKJACK: 
+			if input("Would you like to  draw (y/n) ") == 'y':
+				user+=the_deck.deal_card()
+				user_value=sum([item.get_black_jack_value() for item in user])
+				user_value_ace_1=sum([item.get_black_jack_value(1) for item in user])
+				if user_value > BLACKJACK:
+					user_value=user_value_ace_1
+				print(f"You now have {user} your blackjack total is {user_value}")
+			
+			if (computer_value < (BLACKJACK - 4) and user_value < (BLACKJACK+1)) or (computer_value < user_value and user_value <= BLACKJACK):
+				computer+=the_deck.deal_card()
+				computer_value=sum([item.get_black_jack_value() for item in computer])
+				computer_value_ace_1=sum([item.get_black_jack_value(1) for item in computer])
+				if computer_value > BLACKJACK:
+					computer_value=computer_value_ace_1
+				print(f"The computer has {computer} his blackjack total is {computer_value}")
 
-	if computer_value > BLACKJACK or (user_value <= BLACKJACK and user_value > computer_value):
-		user_won=True
-		user_wins+=1
-		print("You won!")		
-	elif user_value > BLACKJACK or (computer_value <= BLACKJACK and computer_value > user_value):
-		computer_won=True
-		computer_wins+=1
-		print("Computer won!")		
-	else:
-		print("It is a draw.")
+		if computer_value > BLACKJACK or (user_value <= BLACKJACK and user_value > computer_value):
+			user_won=True
+			user_wins+=1
+			print("You won!")		
+		elif user_value > BLACKJACK or (computer_value <= BLACKJACK and computer_value > user_value):
+			computer_won=True
+			computer_wins+=1
+			print("Computer won!")		
+		else:
+			print("It is a draw.")
 
-	print("Computer wins="+str(computer_wins)+" | User wins="+str(user_wins))
+		print("Computer wins="+str(computer_wins)+" | User wins="+str(user_wins))
 
-	if input("Would you like to play again (y/n) ") == 'n':
-		break
+		if input("Would you like to play again (y/n) ") == 'n':
+			break
 
-	print()
+		print()
