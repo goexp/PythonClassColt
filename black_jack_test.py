20c2ed589030f4f0c2aa6ed44a8509c8f6dcf7aa
Ray Valiani <ray.valiani@tsi.com>
2018-04-12 15:35:35 -0500

    Took out template comments in black_jack_test

--- black_jack_test.py
+++ black_jack_test.py
@@ -20,29 +20,6 @@ class Deck_test(unittest.TestCase):
 	def tearDown(self):
 		pass
 
-#    def test_sample(self):
-    	#eg.
-        #self.assertEqual(1, 1)
-        #self.asserRaises(ValueError)
-        #---more options---
-        #assertTrue(a, Message)
-		#assertFalse(a, Message)
-		#assertEqual(a, b, Message)
-		#assertNotEqual(a, b, Message)
-		#assertIs(a, b, Message)
-		#assertIsNot(a, b, Message)
-		#assertIsNone(a, Message)
-		#assertIsNotNone(a, Message)
-		#assertIsInstance(a,b,M)
-		#assertIsNotInstance(a, b, Message)
-		#assertGreater(a, b, Message)
-		#assertLess(a, b, Message)
-		#assertGreaterEqual(a, b, Message)
-		#assertLessEqual(a, b, Message)
-		#assertAlmostEqual(a, b, n, Message)
-		#assertNotAlmostEqual(a, b, n, Message)
-		#assertItemsEqual(a, b, Message)
-
 if __name__ == '__main__':
 	unittest.main()
 		
