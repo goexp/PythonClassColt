344961f2f8617286e3af29686401b8df66bd5a78
Ray Valiani <ray.valiani@tsi.com>
2018-04-25 11:21:23 -0500

    changes to test

--- black_jack_test.py
+++ black_jack_test.py
@@ -16,21 +16,44 @@ class Card_test(unittest.TestCase):
 
 class Deck_test(unittest.TestCase):
 	def setUp(self):
-		pass
+		self.deck=Deck(Card)
+	
 	def tearDown(self):
 		pass
+	
 	def test_init(self):
 		"""Each instance should have a cards attribute with all 52 possible instances of Card"""
-		pass
+		all_cards=[Card(suit,value) for suit in Card.valid_suits for value in Card.valid_values]
+		isEqual=True
+		cards_from_deck = str(self.deck.deal_hand(52))
+		for card in all_cards:
+			if not str(card) in cards_from_deck:
+				isEqual=False
+		self.assertTrue(isEqual)
+
 	def test_count(self):
 		"""Should return how many cards remain in a deck"""
-		pass
+		self.assertEqual(self.deck.count(),52)
+		self.deck.deal_card()
+		self.assertEqual(self.deck.count(),51)
+
 	def test_repr(self):
 		"""Should return how many cards are left in a deck eg. 'Deck of 52 cards'"""
-		pass
+		self.assertEqual(str(self.deck),"Deck of 52 cards")
+		self.deck.deal_card()
+		self.assertEqual(str(self.deck),"Deck of 51 cards")
+	
 	def test_deal(self):
 		"""Accepts a number and removes that many cards from a deck. If there are no cards left it returns a valueError 'All cards have been dealt'"""
-		pass
+		all_cards=self.deck._deal(52)
+		self.assertEqual(self.deck.count(),0)
+		raised_exception=False
+		try:
+			self.deck._deal(1)
+		except ValueError:
+			raised_exception=True
+		self.assertTrue(raised_exception)		
+
 	def test_shuffle(self):
 		"""Should shuffle a full deck. If any are missing it should return a valueError 'Only full decks can be shuffled'"""
 		pass
